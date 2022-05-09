513a2489a3c69c41140e8a190c31e9833f33de12
compiii <jerrylacmou@gmail.com>
2022-05-09 11:47:30 +0100

    Remove

--- matrics_experimentations/2-D_SAP-CGM_Compiii/20220506/exp_2/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module load openmpi/intel-opa/gcc/64/1.10.4-hfi
-
-mpic++ -std=c++1z -Wall -Wextra -O2 -pedantic -c src/*.cpp src/1D/*.cpp src/2D/*.cpp
-mpic++ -o 2D_SAP.run *.o
-rm *.o
--- matrics_experimentations/2-D_SAP-CGM_Compiii/20220506/exp_2/src/2D/classic_seq_2d_sap.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLASSIC_SEQ_2D_SAP_H
-#define CLASSIC_SEQ_2D_SAP_H
-
-#include <string>
-#include <vector>
-
-int classic_seq_2d_sap(const std::vector<std::string> &x, const std::vector<std::string> &y, const std::vector<std::string> &trans_x, const std::vector<std::string> &trans_y, double &preprocessing_time, double &computation_time);
-
-#endif
